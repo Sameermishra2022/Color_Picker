5b805fae551c1f8fa2ced4bbb23f689a421c7e8d
Sameer Mishra <115838087+Sameermishra2022@users.noreply.github.com>
2024-10-08 19:44:28 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,14 @@
+// let displayColor = ()=>{
+//     let color = prompt('Enter Color');
+//     document.body.style.background = color;
+// }
+//or
+let displayColor = ()=>{
+    // let color = prompt('Enter color')
+    // let b = document.getElementsByTagName('body')[0]
+    // b.style.background = color
+
+    //or
+
+    document.body.style.background = prompt('Enter Color')
+}
